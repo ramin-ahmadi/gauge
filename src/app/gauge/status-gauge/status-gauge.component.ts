e3919ac63a2dc72b9780a9028afd6019a4e5701b
Ramin Ahmadi <ramin.ahmadi@faircg.com>
2022-01-23 17:17:22 +1100

    Add trains data and update the code base

--- src/app/gauge/status-gauge/status-gauge.component.ts
+++ src/app/gauge/status-gauge/status-gauge.component.ts
@@ -7,6 +7,8 @@ import { AvailabilityService } from 'src/app/services/availability.service';
 import { TrainService } from 'src/app/services/train.service';
 import { WeighbridgeService } from 'src/app/services/weighbridge.service';
 import Moment from 'moment';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-status-gauge',
   templateUrl: './status-gauge.component.html',
@@ -40,6 +42,8 @@ export class StatusGaugeComponent implements OnInit {
       .subscribe((data) => {
         if (data) {
           this.availabilities = data;
+        } else {
+          alert('there are no availability data')
         }
       });
 
@@ -49,6 +53,8 @@ export class StatusGaugeComponent implements OnInit {
       .subscribe((data) => {
         if (data) {
           this.trains = data;
+        } else {
+          alert('there are no train data')
         }
       });
   }
@@ -57,7 +63,7 @@ export class StatusGaugeComponent implements OnInit {
     return this.weighbridgeService.getStatusColorCode(statusColor);
   }
 
-  public polarToCartesian(centerX:number, centerY:number, radius:number, angleInDegrees:number) {
+  public calculateCircle(centerX:number, centerY:number, radius:number, angleInDegrees:number) {
     var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
   
     return {
@@ -66,28 +72,34 @@ export class StatusGaugeComponent implements OnInit {
     };
   }
   
-  public describeArc(x:number, y:number, radius:number, startAngle:number, endAngle:number): string{
+  public drawArc(x:number, y:number, radius:number, startAngle:number, endAngle:number): string{
   
-      var start = this.polarToCartesian(x, y, radius, endAngle);
-      var end = this.polarToCartesian(x, y, radius, startAngle);
+      var start = this.calculateCircle(x, y, radius, endAngle);
+      var end = this.calculateCircle(x, y, radius, startAngle);
   
-      var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+      var largeArc = endAngle - startAngle <= 180 ? "0" : "1";
   
-      var d = [
+      var svgArcObject = [
           "M", start.x, start.y, 
-          "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+          "A", radius, radius, 0, largeArc, 0, end.x, end.y
       ].join(" ");
-      return d;       
+      return svgArcObject;       
   }
 
   public drawAvailability(fromDateTime?:string, toDateTime?: string):string {
-    console.log(Moment(fromDateTime).format('YYYY-MM-DD HH:mm:ss'),Moment(toDateTime).format('YYYY-MM-DD HH:mm:ss'))
+  //  console.log(Moment(fromDateTime).format('YYYY-MM-DD HH:mm:ss'),Moment(toDateTime).format('YYYY-MM-DD HH:mm:ss'))
     let durationInMinutes=  Math.round(this.getDurationOfAvailability(fromDateTime,toDateTime));
     let startTime = Math.round(this.getStartOfAvailability(fromDateTime))
-    console.log("startTime:", startTime)
-    console.log("Duration:", durationInMinutes)
-    let d: string = this.describeArc(25, 25.25, 25.5, startTime, durationInMinutes+startTime);
+  //  console.log("startTime:", startTime)
+  //  console.log("Duration:", durationInMinutes)
+    let d: string = this.drawArc(25, 25.25, 25.5, startTime, durationInMinutes+startTime);
     return d;
+  }
+
+  public getDurationOfAvailability(fromDateTime?:string,toDateTime?: string): number {
+    let minutes = (Moment(toDateTime).diff(fromDateTime, 'minutes') / 1440);
+    let circleArea = 360 * minutes;
+    return circleArea;
   }
 
   public getStartOfAvailability(fromDateTime?:string): number {
@@ -96,10 +108,27 @@ export class StatusGaugeComponent implements OnInit {
     return circleArea;
   }
 
-  public getDurationOfAvailability(fromDateTime?:string,toDateTime?: string): number {
-    let minutes = (Moment(toDateTime).diff(fromDateTime, 'minutes') / 1440);
-    let circleArea = 360 * minutes;
+  
+  public drawTrainEvent(eventDateTime?:string):string {
+   // console.log(Moment(eventDateTime).format('YYYY-MM-DD HH:mm:ss'))
+    let startTime = Math.round(this.getStartOfAvailability(eventDateTime))
+   // console.log("StartOfTrainEvent:", startTime)
+    let arcPath: string = this.drawArc(25, 27.25, 28.3, startTime, 1+startTime);
+    return arcPath;
+  }
+
+  public getStartOfTrainEvent(eventDateTime?:string): number {
+    let startTime = Math.abs((Moment(eventDateTime).startOf('day').diff(eventDateTime, 'minutes')/ 1440));
+    let circleArea = 360 * startTime;
     return circleArea;
+  }
+
+  public convertUTCToHumanTime(eventDateTime?:string): string {
+   return Moment(eventDateTime).format("HH:mm:ss YYYY-MM-DD"); 
+  }
+
+  public onTrainEventClick(pdfUrl?:string): void{
+    window.open(pdfUrl, '_blank');
   }
 
   ngOnDestroy(): void {
